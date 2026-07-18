6f3726c331657272c28c843b0cf69fd18fbc4f94
artem.rai <tokimuro.hashashi@gmail.com>
2026-07-18 20:13:43 +0400

    add-app

--- assets/js/dataLoader.js
+++ assets/js/dataLoader.js
@@ -1,6 +1,6 @@
 export async function loadPhrases() {
 
-    const response = await fetch("../data/phrases.json");
+    const response = await fetch("data/phrases.json");
 
     if (!response.ok) {
         throw new Error("Не удалось загрузить phrases.json");
